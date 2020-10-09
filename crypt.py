ab17d0f925f6974b379a465063d2898f5fb0d95e
Aleksandr Frolov <48522218+Iskander7d@users.noreply.github.com>
2020-10-09 18:21:29 +0500

    Add files via upload
    add vigenere

--- crypt.py
+++ crypt.py
@@ -2,24 +2,24 @@
 alphabet_eng = [chr(i) for i in range(97,123)]
 
 def is_words_valid(text):
-    tmp = ""  # Буффер для отделения слов от строки(т.к она приходит: втакомвиде)
+    tmp = ""
     A = list(text)
-    Arr = []  # хранение отделеных слов,которые прошли по словарю
-    with open("dictionary.txt") as f:  # открываем словарь
-        file = f.read().splitlines()  # Читаем все строки из словаря и с помощью .splitlines() избавляемся от \n
-        for letter in A:  # проход по строке
-            if letter.isalpha():  # если буква
-                tmp += letter  # записываем её в буффер
-            for word in file:  # Проверяем находится ли наш БУФФЕР СЛОВА в словаре
-                if word == tmp:  # если да то добавляем в список Arr
+    Arr = []
+    with open("dictionary.txt") as f:
+        file = f.read().splitlines()
+        for letter in A:
+            if letter.isalpha():
+                tmp += letter
+            for word in file:
+                if word == tmp:
                     Arr.append(tmp)
-                    tmp = ""  # обнуляем буффер
-                    break  # дальнейший проход по словарю не нужен
+                    tmp = ""
+                    break
         count = 0
-        if len(Arr) == 0:  # Если в этом списке пусто,значит ни одно слово не оказалось в словаре
+        if len(Arr) == 0:
             return False
         for i in range(len(Arr)):
-            if Arr[i] not in file or Arr[i] == "":  # доп проверка,если слово не в словаре
+            if Arr[i] not in file or Arr[i] == "":
                 return False
         return True
 
@@ -46,12 +46,50 @@ def caesar_encrypt(crypted_text, step=1):
     return result, is_words_valid(result)
 
 
+
+def vigenere_crypt(text, keyword):
+    result = ''
+    count = 0
+    for symbol in text:
+        symbol_index = alphabet_eng.index(symbol)
+        if (symbol_index + alphabet_eng.index(keyword[count])) < len(alphabet_eng):
+            result += alphabet_eng[symbol_index + alphabet_eng.index(keyword[count])]
+        else:
+            result += alphabet_eng[symbol_index - len(alphabet_eng) + alphabet_eng.index(keyword[count])]
+        count += 1
+        if count >= len(keyword):
+            count = 0
+
+    return result
+
+def vigenere_encrypt(crypted_text, keyword):
+    result = ''
+    count = 0
+    for symbol in crypted_text:
+        symbol_index = alphabet_eng.index(symbol)
+        if (symbol_index - alphabet_eng.index(keyword[count])) >= 0:
+            result += alphabet_eng[symbol_index - alphabet_eng.index(keyword[count])]
+        else:
+            result += alphabet_eng[symbol_index - alphabet_eng.index(keyword[count]) + len(alphabet_eng)]
+        count += 1
+        if count >= len(keyword):
+            count = 0
+
+    return result, is_words_valid(result)
+
+
+def playfair_crypt():
+    pass
+
+def playfair_encrypt():
+    pass
+
 def main():
 
-    default = 'toand'
-    crypted= caesar_crypt(default)
-    encrypted, is_valid = caesar_encrypt(crypted)
-
+    default = 'intheend'
+    keyword = 'hello'
+    crypted = vigenere_crypt(default, keyword)
+    encrypted, is_valid = vigenere_encrypt(crypted, keyword)
     print(crypted, encrypted, is_valid)
 
 if __name__ == '__main__':
